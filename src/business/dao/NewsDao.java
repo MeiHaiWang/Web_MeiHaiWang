0e3b2826fbab68d0a6f781ad41437ac8346ecfc9
ka2jun8 <kanijunnari@kanijunnari-no-MacBook-Air.local>
2015-05-29 01:43:02 +0900

    news部分
    作りたかったけど、もう眠いから明日に…

---
+++ src/business/dao/NewsDao.java
@@ -0,0 +1,42 @@
+package business.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import common.model.BeautyNewsInfo;
+import common.util.DBConnection;
+
+public class NewsDao {
+	
+	public NewsDao() throws Exception{
+		
+	}
+
+	public List<BeautyNewsInfo> getBeautyNewsInfo(DBConnection dbConnection) throws SQLException{
+		String sql = "SELECT `t_masterNewsName`, `t_masterNewImagePath`, `t_masterNewsURL` FROM `t_masterNews`)";
+		List<BeautyNewsInfo> BeautyNewsInfoList = new ArrayList<BeautyNewsInfo>();
+		
+		Statement statement = dbConnection.getStatement();
+		try {
+			ResultSet rs = statement.executeQuery(sql);
+			while(rs.next()){
+				BeautyNewsInfo BeautyNewsInfo = new BeautyNewsInfo();
+				//BeautyNewsInfo.setBeautyNewsId(rs.getInt("t_masterNewsId"));
+				BeautyNewsInfo.setbeautyNewsName(rs.getString("t_masterNewsName"));
+				BeautyNewsInfo.setbeautyNewsImagePath(rs.getString("t_masterNewImagePath"));
+				BeautyNewsInfo.setbeautyNewsURL(rs.getString("t_masterNewsURL"));
+				BeautyNewsInfoList.add(BeautyNewsInfo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
+		return BeautyNewsInfoList;
+	}	
+	
+}
