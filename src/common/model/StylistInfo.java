d2fe7e72f9f9fa5449045343e7d73c02c6617a03
ka2jun8 <kanijunnari@kanijunnari-no-MacBook-Air.local>
2015-05-31 12:40:19 +0900

    StyleInfo now.

--- src/common/model/StylistInfo.java
+++ src/common/model/StylistInfo.java
@@ -7,6 +7,9 @@ public class StylistInfo {
 	private int salonId = Integer.MIN_VALUE;
 	private int isgood = 0;
 	private int favoriteNumber = Integer.MIN_VALUE;
+	private int stylistGender = Integer.MIN_VALUE;
+	private int yearsNumber = Integer.MIN_VALUE;
+	private String stylistMessage="";
 	
 	public StylistInfo(){
 		
@@ -17,6 +20,14 @@ public class StylistInfo {
 	}
 	
 	public int getStylistId(){
+		return stylistId;
+	}
+	
+	public void setStylistGender(int stylistGender){
+		this.stylistGender = stylistGender;
+	}
+
+	public int getStylistGender(){
 		return stylistId;
 	}
 	
@@ -59,4 +70,21 @@ public class StylistInfo {
 	public int getFavoriteNumber(){
 		return favoriteNumber;
 	}
+
+	public void setStylistYears(int yearsNumber){
+		this.yearsNumber = yearsNumber;
+	}
+	
+	public int getStylistYearsNumber(){
+		return yearsNumber;
+	}
+
+	public void setStylistMessage(String stylistMessage){
+		this.stylistMessage = stylistMessage != null ? stylistMessage : "";
+	}
+	public String getStylistMessage(){
+		return stylistMessage;
+	}
+
+	
 }
