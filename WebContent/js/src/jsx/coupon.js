1d53d2d513f0f406816ba882fb248e38ed4b7f9f
do7be <bomsap700@gmail.com>
2015-07-12 12:29:10 +0900

    クーポン情報管理画面にて画像アップロードできるように

--- WebContent/js/src/jsx/coupon.js
+++ WebContent/js/src/jsx/coupon.js
@@ -296,12 +296,8 @@ $(function(){
   $('#coupon_image').change(function() {
     // ファイルが選択されたか
     if($(this).prop('files')[0]){
-      var fd = new FormData($('#update')[0]);
+      var data = new FormData($('#update')[0]);
 
-      var data = {
-        t_hairSalonMaster_salonId: session_info.t_hairSalonMaster_salonId,
-        file: fd,
-      }
       var result = uploadImage(data);
       if (result.result == "true") {
         component_coupon_image_path.setState({t_coupon_imagePath: result.image_path});
