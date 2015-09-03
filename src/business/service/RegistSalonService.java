7fcc936184a46e026018f447d24646e11da73c3a
HirokiEbina <Tokyo>
2015-09-04 00:34:59 +0900

    メール一部　不要なファイル削除
    アップロード画像　不要　削除

--- src/business/service/RegistSalonService.java
+++ src/business/service/RegistSalonService.java
@@ -1,19 +1,24 @@
 package business.service;
 
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.Authenticator;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
-import business.dao.AreaDao;
 import business.dao.SalonDao;
-import common.model.AreaInfo;
 import common.model.HairSalonInfo;
+import common.util.ConfigUtil;
 import common.util.DBConnection;
+import common.util.MailAuthenticator;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class RegistSalonService {
 	public HttpServletResponse excuteService(HttpServletRequest request,
@@ -75,7 +80,40 @@ public class RegistSalonService {
 		    PrintWriter out = response.getWriter();
 		    out.print(jsonObject);
 		    out.flush();
-		    
+		    try{
+/*	            Properties property = new Properties();
+	            property.put("mail.smtp.auth", "true");
+	            property.put("mail.smtp.starttls.enable", "true");
+	            property.put("mail.smtp.host", ConfigUtil.getConfig("smtp_hostName"));
+	            property.put("mail.smtp.port", ConfigUtil.getConfig("smtp_portNum"));
+	            property.put("mail.smtp.debug", "true");
+	            MailAuthenticator mailCreator = new MailAuthenticator(ConfigUtil.getConfig("smtp_userName"),ConfigUtil.getConfig("smtp_password"));
+	            Session session = Session.getInstance(property,mailCreator);
+	            
+	            MimeMessage mimeMessage = new MimeMessage(session);
+
+	            InternetAddress toAddress = new InternetAddress(mail, contactUserName);
+
+	            mimeMessage.setRecipient(Message.RecipientType.TO, toAddress);
+
+	            InternetAddress fromAddress = new InternetAddress(ConfigUtil.getConfig("smtp_senderAddress"),ConfigUtil.getConfig("smtp_senderName"));
+
+	            mimeMessage.setFrom(fromAddress);
+
+	            mimeMessage.setSubject("たいとっる", "ISO-2022-JP");
+
+	            mimeMessage.setText("ほんぶーん", "ISO-2022-JP");
+
+	            Transport.send(mimeMessage);
+
+	            out.println("<htm><body>");
+	            out.println("■お問い合わせ内容を担当者へ送信しました。");
+	            out.println("<body></html>");*/
+		    }catch(Exception e){
+		    	responseStatus = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
+		    	e.printStackTrace();
+		    }
+               
 		}catch(Exception e){
 			responseStatus = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 			e.printStackTrace();
